ad49d47861b02ab24f9f8f916033a804136bb701
Nguyen Quoc Anh <quoccun123@gmail.com>
2025-03-19 11:46:01 +0700

    Map.cpp 0.1

--- Map.cpp
+++ Map.cpp
@@ -3,8 +3,18 @@
 void GameMap::LoadMap (char* name)
 {
     std::ifstream InputFile(;);
-    if(!InputFile){
+    if(!(InputFile.is_open()){
         return;
     }
+
+}
+
+void GameMap::LoadTiles (SDL_Renderer* screen)
+{
+
+}
+
+void GameMap::DrawMap (SDL_Renderer* screen)
+{
 
 }
